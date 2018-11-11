c3efeb78ca1117c01ec13095628f920a7aa4e587
sofimarquez22 <sofiamarquez@Sofias-MacBook-Air.local>
2018-11-10 18:00:13 -0800

    Added to the main a class for Sprites

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import pygame
+from pygame.locals import *
+import math
 
+class Main():
+    def __init__(self):
+        self.list=[]
+        for i in range(150):
+            x=random.randrange(0,800)
+            y=random.randrange(0,600)
+            self.list.append([x,y])
+
+class Zombie(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("zombie".png").convert()
+        self.image.set_colorkey((255, 255, 255))
