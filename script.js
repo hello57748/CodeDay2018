79d705de72a5d9e53ed7763147c04c802fa31c22
hello57748 <sampeters@csumb.edu>
2018-11-11 09:50:56 -0800

    now its a website

---
+++ script.js
@@ -0,0 +1,3 @@
+var BoyNames = ['Liam','Noah','William','James','Logan','Benjamin'];
+var randomNumber = Math.floor(Math.random()*textArray.length);
+audioElement.setAttribute('src', textArray[randomNumber]);
